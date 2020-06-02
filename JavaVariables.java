00c15933d7b8964bf6443fdad94f50af2cdc1c1b
Hexseral <54594278+Hexseral@users.noreply.github.com>
2020-06-01 21:48:01 -0500

    Update JavaVariables.java

--- JavaVariables.java
+++ JavaVariables.java
@@ -1,63 +1,62 @@
-/* Jordan Tatum
- * This program is a demonstration of the course material we have covered so far within COP 2006.
- * I will be demonstrating the different Data Types within Java, but first we must understand what a Variable
- * is. A variable is essentially a container that holds values within a java program, and a variable can be 
- * declared to use one of the main eight data types offered in Java. The eight data types demonstrated in this 
- * program are Byte, Short, Int, Long, Float, Double, Char, String, and Boolean. Another important concept
- * is scope, and within Java scope refers to the visibility of a variable within a program. Scope is important
- * when building objects due to any variable defined within a class outside of any method can be used by 
- * all member methods. Scope and Variables are two important concepts to understand Object Oriented Programming.
- * See Below:
- */
- class JavaVariables {
-	public static void main(String[] args) {
-		
-		//Byte: 8 bit int, min -128 & max 127. Useful for conserving memory.
-		byte bus;
-		 bus = 100;
-		   
-		//Short: 16 bit int, min -32,768 & max 32,767. Useful for conserving memory.
-		 short snails; 
-		 snails = 1000;
-		
-		//Int: 32 bit int, -2^31 min & max of (2^31)-1. Great for basic arithmetic without fractional parts, or insane size.
-		 int insects;
-		 insects = 100000;
-		
-		//Long: 64 bit int, -2^63 min & max of (2^63)-1. Like an int except much larger range.
-		 long Lice; 
-		 Lice = 123_456L;
-		
-		//Float: 32 bit IEEE, Range beyond scope of discussion. Used for fractional parts (decimals)
-		 float fish;
-		 fish = 123.4f;
-		
-		//Double: 64 bit IEEE, Range beyond scope of discussion. like a float but much more accurate in terms of it's fractional parts (decimals)
-		double dog;
-		dog = 123.4 ;
-		
-		//char: 16 bit unicode, Stores character data (strings)
-		 char cat;
-		 cat = 'c'; 
-		
-		//boolean: Strictly used for true or false outcome storage within variables.
-		 boolean result;
-		 result = false;
-		 
-		//Final: A Final Acts as a constant variable within a program and cannot be manipulated or changed. 
-		 final int intFinal;
-		 intFinal = 150; 
-		
-		System.out.println("Hello User Welcome To the Data Type Demonstration!");
-		System.out.println("An example of a Byte Stored Within This Program: " + bus);
-		System.out.println("An example of a Short Stored Within This Program: " + snails);
-		System.out.println("An example of a Int Stored Within This Program: " + insects);
-		System.out.println("An example of a Long Stored Within This Program: "+ Lice);
-		System.out.println("An example of a Float Stored Within This Program: " + fish);
-		System.out.println("An example of a Double Stored Within This Program: " + dog);
-		System.out.println("An example of a Char Stored Within This Program: " + cat);
-		System.out.println("An example of a Boolean Stored Within This Program: " + result);
-		System.out.println("An example of a Final Int Stored Within This Program: " + intFinal);
-	}
-	
+// Jordan Tatum
+
+// This imports our math & random packages
+import java.lang.Math;
+import java.util.Random;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+
+    // this is our argument (argument values)
+    int sum = add(28, 32);
+    System.out.println("Ex1)  This is a method deomonstration: " + sum);
+
+    // this uses our ranSum method to demonstrate how we can use
+    int randSum = ranSum();
+    System.out.println(
+        "Ex2)  This is a method deomonstration of adding two randomly generated numbers between 1-50: "
+            + randSum);
+
+    int rootSum = root();
+    System.out.println(
+        "Ex3)  This is a method demonstration of adding two square rooted numbers: " + rootSum);
+
+    // This if else statement evaluates the randSum & rootSum and prints out the greater value
+    if (rootSum > randSum) {
+      System.out.println("Example two has a larger sum then example 1");
+    } else {
+      System.out.println("Example one as a larger sum then example 2");
+    }
+    
+    // this is an example of using a ternary construct
+    int max = randSum >= rootSum ? randSum : rootSum;
+      System.out.println("Java Ternary example: "+max);
+
+  }
+
+  // This is a method header with a parameter list within the parenthesis
+  public static int add(int i, int j) {
+    // This is our return value
+    return i + j;
+
+  }
+
+  public static int ranSum() {
+    // This is my demonstration of using the Random class
+    Random rand = new Random();
+    int randOne = +rand.nextInt(50);
+    int randTwo = +rand.nextInt(50);
+    return randOne + randTwo;
+  }
+
+  public static int root() {
+    double num1 = 24;
+    double num2 = 64;
+    /*
+     * This is an example of using the Math class to return the sum of two square roots while also
+     * casting the sum from a double to an integer.
+     */
+    return (int) (Math.sqrt(num1) + Math.sqrt(num2));
+  }
 }
+
